638e26dea7f773eaa5e3b5c157859f36758d0377
Brandon B <titusrevised@gmail.com>
2017-04-28 14:13:01 -0700

    fixed tags error

--- pelicanconf.py
+++ pelicanconf.py
@@ -43,7 +43,7 @@ THEME = 'blue-penguin'
 
 MARKUP = ('md', 'ipynb')
 
-STATIC_PATHS = ['images', 'pdfs', 'pages', 'js']
+STATIC_PATHS = ['images', 'pdfs', 'pages', 'js', 'extra']
 STATIC_IMG = 'images'
 CONTACT_EMAIL = 'outlacedev@gmail.com'
 
@@ -75,15 +75,32 @@ DISPLAY_HOME   = True
 DISPLAY_MENU   = True
 
 # provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
-TAGS_URL           = 'tags'
+'''TAGS_URL           = 'tags'
 TAGS_SAVE_AS       = 'tags/index.html'
 #AUTHORS_URL        = 'authors'
 #AUTHORS_SAVE_AS    = 'authors/index.html'
 CATEGORIES_URL     = 'categories'
 CATEGORIES_SAVE_AS = 'categories/index.html'
 ARCHIVES_URL       = 'archives'
-ARCHIVES_SAVE_AS   = 'archives/index.html'
+ARCHIVES_SAVE_AS   = 'archives/index.html'''
 
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_URL = 'tags/'
+TAGS_SAVE_AS = 'tags/index.html'
+
+AUTHOR_URL = 'author/{slug}/'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_URL = 'authors/'
+AUTHORS_SAVE_AS = 'authors/index.html'
+
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_URL = 'categories/'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+
+ARCHIVES_URL       = 'archives/{slug}/'
+ARCHIVES_SAVE_AS   = 'archives/{slug}/index.html'
 
 # use those if you want pelican standard pages to appear in your menu
 MENU_INTERNAL_PAGES = (
