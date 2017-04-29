798493ac6402683c8f85ee6dad85b0adb91d6c44
Brandon B <titusrevised@gmail.com>
2017-04-28 17:43:27 -0700

    fix links

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Brandon'
 SITENAME = 'Δ ℚuantitative √ourney'
-#SITEURL = 'http://outlace.com'
+SITEURL = 'http://outlace.com'
 
 PATH = 'content'
 
